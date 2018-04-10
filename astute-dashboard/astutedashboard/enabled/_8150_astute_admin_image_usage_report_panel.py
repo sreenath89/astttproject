0175cd5a23a0cb63d06f850749753ed330aa781d
sreenathmmenon <zreenathmenon@gmail.com>
2018-04-10 14:56:58 +0530

    Adding enabled file

---
+++ astute-dashboard/astutedashboard/enabled/_8150_astute_admin_image_usage_report_panel.py
@@ -0,0 +1,6 @@
+PANEL_GROUP = 'default'
+PANEL_DASHBOARD = 'billing'
+PANEL = 'image_usage_report'
+
+# Python panel class of the PANEL to be added.
+ADD_PANEL = 'astutedashboard.dashboards.billing.image_report.panel.ImageUsageMappings'
